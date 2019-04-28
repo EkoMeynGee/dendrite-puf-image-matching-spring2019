f5d6887f88afc34f07a9d6d82493e5847fff08e1
Zaoyi <zc95@nau.edu>
2019-04-27 17:30:45 -0700

    new sh(1-30 , 10sp noise)

---
+++ nnn24.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=readmat
+#SBATCH --output=/home/zc95/dendrite-puf-monsoon-results2/out24.txt
+#SBATCH --error=/home/zc95/dendrite-puf-monsoon-results2/error24.txt
+#SBATCH --time=24:00:00
+#SBATCH --workdir=/home/zc95/dendrite-puf-image-matching-first2
+#SBATCH --mem=60000
+#SBATCH -c6
+
+module load matlab
+
+matlab -nodisplay -nodesktop -r "run noiseSript24.m"
+srun date
---
+++ nnn25.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=readmat
+#SBATCH --output=/home/zc95/dendrite-puf-monsoon-results2/out25.txt
+#SBATCH --error=/home/zc95/dendrite-puf-monsoon-results2/error25.txt
+#SBATCH --time=24:00:00
+#SBATCH --workdir=/home/zc95/dendrite-puf-image-matching-first2
+#SBATCH --mem=60000
+#SBATCH -c6
+
+module load matlab
+
+matlab -nodisplay -nodesktop -r "run noiseSript25.m"
+srun date
