fc7fac2eeb07ba613fe366ef66c4dd01515d8b3a
Zaoyi <zc95@nau.edu>
2019-05-06 10:30:33 -0700

    second skewness set

--- results_skewness/skew_feature_map.m
+++ results_skewness/skew_feature_map.m
@@ -1,6 +1,6 @@
-numbersamples = 50;
+numbersamples = 100;
 
-refIm = 'b6.png';
+refIm = 'b7.png';
 ref = colorextract(refIm);
 para_ref = round(mean(hough_circle(ref, .5, .1, 33, 37, 1),2));
 circleRef.x = para_ref(2);
@@ -11,10 +11,10 @@ circleRef.radius = para_ref(3);
 
 rate = zeros(numbersamples,1);
 
-for index = 1:1
-    
-    
-    eval(['filename = "sk' num2str(index) '.png";']);
+for index = 1:numbersamples
+
+    eval(['filename = "s7k' num2str(index) '.png";']);
+    filename = char(filename);
     try
         im_t = colorextract(filename);
         para_test = round(mean(hough_circle(im_t, .5, .1, 33, 37, 1),2));
